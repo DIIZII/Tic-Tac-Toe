59578c230cb40de15be1f2a1bfd89b39fe3fcda0
Dmitry <38500408+DIIZII@users.noreply.github.com>
2020-10-20 00:20:10 +0300

    add game without mainwindow
    player VS player

--- Tic_Tac_Toe/playwindow.cpp
+++ Tic_Tac_Toe/playwindow.cpp
@@ -11,6 +11,9 @@ PlayWindow::PlayWindow(QWidget *parent) :
     for(auto tmp_x = 0; tmp_x < Size_x; tmp_x++)
         for(auto tmp_y = 0; tmp_y < Size_y; tmp_y++)
             matrix[tmp_x][tmp_y] = -1;
+
+    winZero = 0;
+    winCross = 0;
 }
 
 PlayWindow::~PlayWindow()
@@ -32,6 +35,7 @@ std::shared_ptr<PlayWindow> PlayWindow::instance()
 
 void PlayWindow::on_pushButton_clicked()
 {
+    ui->pushButton->setEnabled(false);
     GamePlayerVSPlayer(ui->pushButton,0,0);
 //    QPixmap pix(":/res/img/x.jpg");
 //    int w = ui->pushButton->width();
@@ -42,6 +46,7 @@ void PlayWindow::on_pushButton_clicked()
 
 void PlayWindow::on_pushButton_2_clicked()
 {
+    ui->pushButton_2->setEnabled(false);
     GamePlayerVSPlayer(ui->pushButton_2,0,1);
 //    QPixmap pix(":/res/img/x.jpg");
 //    int w = ui->pushButton_2->width();
@@ -53,6 +58,7 @@ void PlayWindow::on_pushButton_2_clicked()
 
 void PlayWindow::on_pushButton_3_clicked()
 {
+    ui->pushButton_3->setEnabled(false);
     GamePlayerVSPlayer(ui->pushButton_3,0,2);
 //    QPixmap pix(":/res/img/x.jpg");
 //    int w = ui->pushButton_3->width();
@@ -63,6 +69,7 @@ void PlayWindow::on_pushButton_3_clicked()
 
 void PlayWindow::on_pushButton_4_clicked()
 {
+    ui->pushButton_4->setEnabled(false);
     GamePlayerVSPlayer(ui->pushButton_4,1,0);
 //    QPixmap pix(":/res/img/x.jpg");
 //    int w = ui->pushButton_4->width();
@@ -73,6 +80,7 @@ void PlayWindow::on_pushButton_4_clicked()
 
 void PlayWindow::on_pushButton_5_clicked()
 {
+    ui->pushButton_5->setEnabled(false);
     GamePlayerVSPlayer(ui->pushButton_5,1,1);
 //    QPixmap pix(":/res/img/o.jpg");
 //    int w = ui->pushButton_5->width();
@@ -83,6 +91,7 @@ void PlayWindow::on_pushButton_5_clicked()
 
 void PlayWindow::on_pushButton_6_clicked()
 {
+    ui->pushButton_6->setEnabled(false);
     GamePlayerVSPlayer(ui->pushButton_6,1,2);
 //    QPixmap pix(":/res/img/x.jpg");
 //    int w = ui->pushButton_6->width();
@@ -93,6 +102,7 @@ void PlayWindow::on_pushButton_6_clicked()
 
 void PlayWindow::on_pushButton_7_clicked()
 {
+    ui->pushButton_7->setEnabled(false);
     GamePlayerVSPlayer(ui->pushButton_7,2,0);
 //    QPixmap pix(":/res/img/x.jpg");
 //    int w = ui->pushButton_7->width();
@@ -103,6 +113,7 @@ void PlayWindow::on_pushButton_7_clicked()
 
 void PlayWindow::on_pushButton_8_clicked()
 {
+    ui->pushButton_8->setEnabled(false);
     GamePlayerVSPlayer(ui->pushButton_8,2,1);
 //    QPixmap pix(":/res/img/x.jpg");
 //    int w = ui->pushButton_8->width();
@@ -113,6 +124,7 @@ void PlayWindow::on_pushButton_8_clicked()
 
 void PlayWindow::on_pushButton_9_clicked()
 {
+    ui->pushButton_9->setEnabled(false);
     GamePlayerVSPlayer(ui->pushButton_9,2,2);
 //    QPixmap pix(":/res/img/x.jpg");
 //    int w = ui->pushButton_9->width();
@@ -144,65 +156,139 @@ void PlayWindow::GamePlayerVSPlayer(QPushButton *button, int x, int y)
     }
 //    if((move == 9 || Roles()) || (move == 9 && Roles()))
 
-    if(Roles() == true)
+    int result = -1;
+    if(Roles(result) == true)
     {
-        QMessageBox::about(this,"Победа","Новая партия");
-        move = 0;
-        ui->pushButton->setIcon(QIcon());
-        ui->pushButton_2->setIcon(QIcon());
-        ui->pushButton_3->setIcon(QIcon());
-        ui->pushButton_4->setIcon(QIcon());
-        ui->pushButton_5->setIcon(QIcon());
-        ui->pushButton_6->setIcon(QIcon());
-        ui->pushButton_7->setIcon(QIcon());
-        ui->pushButton_8->setIcon(QIcon());
-        ui->pushButton_9->setIcon(QIcon());
-        for(auto tmp_x = 0; tmp_x < Size_x; tmp_x++)
-            for(auto tmp_y = 0; tmp_y < Size_y; tmp_y++)
-                matrix[tmp_x][tmp_y] = -1;
+//        QMessageBox::about(this,"Победа","Новая партия");
+        if(result == 1)
+        {
+            winCross++;
+            QMessageBox::about(this,"Победа","Победили крестики. Счет"
+                               + QString::number(winCross)
+                               + ":"
+                               + QString::number(winZero)
+                               + "\nНовая партия");
+        }
+        else if(result == 0)
+        {
+            winZero++;
+            QMessageBox::about(this,"Победа","Победили нолики. Счет"
+                               + QString::number(winCross)
+                               + ":"
+                               + QString::number(winZero)
+                               + "\nНовая партия");
+        }
+        else
+        {
+            //-1
+        }
+        NewGame();
     }
 
     if(move == 9)
     {
-        QMessageBox::StandardButton reply = QMessageBox::question(this,"Закончились ходы","Начать сначало",QMessageBox::Yes|QMessageBox::No);
+        QMessageBox::StandardButton reply = QMessageBox::question(this,"Закончились ходы","Ничья.))) \nНачать сначало",QMessageBox::Yes|QMessageBox::No);
         if(reply == QMessageBox::Yes)
         {
-            move = 0;
-            ui->pushButton->setIcon(QIcon());
-            ui->pushButton_2->setIcon(QIcon());
-            ui->pushButton_3->setIcon(QIcon());
-            ui->pushButton_4->setIcon(QIcon());
-            ui->pushButton_5->setIcon(QIcon());
-            ui->pushButton_6->setIcon(QIcon());
-            ui->pushButton_7->setIcon(QIcon());
-            ui->pushButton_8->setIcon(QIcon());
-            ui->pushButton_9->setIcon(QIcon());
-            for(auto tmp_x = 0; tmp_x < Size_x; tmp_x++)
-                for(auto tmp_y = 0; tmp_y < Size_y; tmp_y++)
-                    matrix[tmp_x][tmp_y] = -1;
+            NewGame();
         }
     }
 }
 
-bool PlayWindow::Roles()
+void PlayWindow::NewGame()
 {
-    if((matrix[0][0] == matrix[0][1] == matrix[0][2] && matrix[0][0] != -1 && matrix[0][1] != -1 && matrix[0][2] != -1))
+    move = 0;
+    ui->pushButton->setIcon(QIcon());
+    ui->pushButton_2->setIcon(QIcon());
+    ui->pushButton_3->setIcon(QIcon());
+    ui->pushButton_4->setIcon(QIcon());
+    ui->pushButton_5->setIcon(QIcon());
+    ui->pushButton_6->setIcon(QIcon());
+    ui->pushButton_7->setIcon(QIcon());
+    ui->pushButton_8->setIcon(QIcon());
+    ui->pushButton_9->setIcon(QIcon());
+    ui->pushButton->setEnabled(true);
+    ui->pushButton_2->setEnabled(true);
+    ui->pushButton_3->setEnabled(true);
+    ui->pushButton_4->setEnabled(true);
+    ui->pushButton_5->setEnabled(true);
+    ui->pushButton_6->setEnabled(true);
+    ui->pushButton_7->setEnabled(true);
+    ui->pushButton_8->setEnabled(true);
+    ui->pushButton_9->setEnabled(true);
+    for(auto tmp_x = 0; tmp_x < Size_x; tmp_x++)
+        for(auto tmp_y = 0; tmp_y < Size_y; tmp_y++)
+            matrix[tmp_x][tmp_y] = -1;
+}
+
+bool PlayWindow::Roles(int &number)
+{
+    if((matrix[0][0] == 0 && matrix[0][1] == 0 && matrix[0][2] == 0)
+            || (matrix[1][0] == 0 && matrix[1][1] == 0 && matrix[1][2] == 0)
+            || (matrix[2][0] == 0 && matrix[2][1] == 0 && matrix[2][2] == 0)
+            || (matrix[0][0] == 0 && matrix[1][0] == 0 && matrix[2][0] == 0)
+            || (matrix[0][1] == 0 && matrix[1][1] == 0 && matrix[2][1] == 0)
+            || (matrix[0][2] == 0 && matrix[1][2] == 0 && matrix[2][2] == 0)
+            || (matrix[0][0] == 0 && matrix[1][1] == 0 && matrix[2][2] == 0)
+            || (matrix[0][2] == 0 && matrix[1][1] == 0 && matrix[2][0] == 0))
+    {
+        number = 0;
         return true;
+    }
+    else if((matrix[0][0] == 1 && matrix[0][1] == 1 && matrix[0][2] == 1)
+            || (matrix[1][0] == 1 && matrix[1][1] == 1 && matrix[1][2] == 1)
+            || (matrix[2][0] == 1 && matrix[2][1] == 1 && matrix[2][2] == 1)
+            || (matrix[0][0] == 1 && matrix[1][0] == 1 && matrix[2][0] == 1)
+            || (matrix[0][1] == 1 && matrix[1][1] == 1 && matrix[2][1] == 1)
+            || (matrix[0][2] == 1 && matrix[1][2] == 1 && matrix[2][2] == 1)
+            || (matrix[0][0] == 1 && matrix[1][1] == 1 && matrix[2][2] == 1)
+            || (matrix[0][2] == 1 && matrix[1][1] == 1 && matrix[2][0] == 1))
+    {
+        number = 1;
+        return true;
+    }
     else
         return false;
-
-//    if((matrix[0][0] == matrix[0][1] == matrix[0][2]) ||
-//            (matrix[1][0] == matrix[1][1] == matrix[1][2]) ||
-//            (matrix[2][0] == matrix[2][1] == matrix[2][2]) ||
-//            (matrix[0][0] == matrix[1][0] == matrix[2][0]) ||
-//            (matrix[0][1] == matrix[1][1] == matrix[2][1]) ||
-//            (matrix[0][2] == matrix[1][2] == matrix[2][2]) ||
-//            (matrix[0][0] == matrix[1][1] == matrix[2][2]) ||
-//            (matrix[0][2] == matrix[1][1] == matrix[2][0]))
-//    {
-//        return true;
-//    }
-//    else
-//        return false;
+/*
+    if((matrix[0][0] == matrix[0][1] == matrix[0][2])
+                        || (matrix[1][0] == matrix[1][1] == matrix[1][2])
+                        || (matrix[2][0] == matrix[2][1] == matrix[2][2])
+                        || (matrix[0][0] == matrix[1][0] == matrix[2][0])
+                        || (matrix[0][1] == matrix[1][1] == matrix[2][1])
+                        || (matrix[0][2] == matrix[1][2] == matrix[2][2])
+                        || (matrix[0][0] == matrix[1][1] == matrix[2][2])
+                        || (matrix[0][2] == matrix[1][1] == matrix[2][0]))
+    {
+        return true;
+    }
+    else
+        return false;
+    if((matrix[0][0] == matrix[0][1] == matrix[0][2] == 0)
+            || (matrix[1][0] == matrix[1][1] == matrix[1][2] == 0)
+            || (matrix[2][0] == matrix[2][1] == matrix[2][2] == 0)
+            || (matrix[0][0] == matrix[1][0] == matrix[2][0] == 0)
+            || (matrix[0][1] == matrix[1][1] == matrix[2][1] == 0)
+            || (matrix[0][2] == matrix[1][2] == matrix[2][2] == 0)
+            || (matrix[0][0] == matrix[1][1] == matrix[2][2] == 0)
+            || (matrix[0][2] == matrix[1][1] == matrix[2][0] == 0))
+    {
+        number = 0;
+        return true;
+    }
+    else if((matrix[0][0] == matrix[0][1] == matrix[0][2] == 1)
+            || (matrix[1][0] == matrix[1][1] == matrix[1][2] == 1)
+            || (matrix[2][0] == matrix[2][1] == matrix[2][2] == 1)
+            || (matrix[0][0] == matrix[1][0] == matrix[2][0] == 1)
+            || (matrix[0][1] == matrix[1][1] == matrix[2][1] == 1)
+            || (matrix[0][2] == matrix[1][2] == matrix[2][2] == 1)
+            || (matrix[0][0] == matrix[1][1] == matrix[2][2] == 1)
+            || (matrix[0][2] == matrix[1][1] == matrix[2][0] == 1))
+    {
+        number = 1;
+        return true;
+    }
+    else
+        return false;
+*/
 }
 
